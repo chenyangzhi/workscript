254c411e7728531da8949c3fe35204759f4202d5
695611691@qq.com <root@0bf607aad758.(none)>
2015-12-17 11:34:22 -0500

    right work

--- stormkafkamon/monitor.py
+++ stormkafkamon/monitor.py
@@ -5,20 +5,73 @@ import sys
 from prettytable import PrettyTable
 import requests
 import simplejson as json
-import re
+import re, urllib
+import smtplib
 
 from zkclient import ZkClient, ZkError
 from processor import process, ProcessorError
 
+url = "http://10.10.10.169:8081/topology.html?id="
+crawlUrl =  "http://10.10.10.169:8081"
+sender = 'coco@zhangwenchao177.jx.diditaxi.com'
+receivers = ['695611691@qq.com']
+message = """From: From coco <coco@zhangwenchao177.jx.diditaxi.com>
+To: To chenyangzhi <695611691@qq.com>
+MIME-Version: 1.0
+Content-type: text/html
+Subject: SMTP HTML e-mail test
+
+This is an e-mail message to be sent in HTML format
+"""
+htmlEnd = """
+</table>
+</body>
+</html>
+"""
+
 def sizeof_fmt(num):
-    for x in [' bytes','KB','MB','GB']:
-        if num < 1024.0:
-            return "%3.1f%s" % (num, x)
+        for x in [' bytes','KB','MB','GB']:
+                if num < 1024.0:
+                        return "%3.1f%s" % (num, x)
         num /= 1024.0
-    return "%3.1f%s" % (num, 'TB')
+        return "%3.1f%s" % (num, 'TB')
 
 def null_fmt(num):
-    return num
+        return num
+def strconcat(np):
+    str1 = ""
+    print "fsafs" 
+    print type(str(np.SecondBoltsProcessLatency))
+
+    str1 = str1 + \
+          "<td>" + str(np.broker)                     +   "</td>"  + \
+          "<td>" + str(np.topic)                      +   "</td>"  + \
+          "<td>" + str(np.partition)                  +   "</td>"  + \
+          "<td>" + str(np.earliest)                   +   "</td>"  + \
+          "<td>" + str(np.latest)                     +   "</td>"  + \
+          "<td>" + str(np.depth)                      +   "</td>"  + \
+          "<td>" + str(np.spout)                      +   "</td>"  + \
+          "<td>" + str(np.current)                    +   "</td>"  + \
+          "<td>" + str(np.delta)                      +   "</td>"  + \
+          "<td>" + str(np.TopologyStatsAck)           +   "</td>"  + \
+          "<td>" + str(np.TopologyStatsFailed)        +   "</td>"  + \
+          "<td>" + str(np.SpoutsAck)                  +   "</td>"  + \
+          "<td>" + str(np.SpoutsFailed)               +   "</td>"  + \
+          "<td>" + str(np.FirstBoltsAck)              +   "</td>"  + \
+          "<td>" + str(np.FirstBoltsFailed)           +   "</td>"  + \
+          "<td>" + str(np.FirstBoltsProcessLatency)   +   "</td>"  + \
+          "<td>" + str(np.SecondBoltsAck)             +   "</td>"  + \
+          "<td>" + str(np.SecondBoltsFailed)          +   "</td>"  + \
+          "<td>" + str(np.SecondBoltsProcessLatency)  +   "</td>"  
+    return str1
+
+def mailmessage(summary):
+    str = ""
+    for p in summary.partitions:
+        str = "<tr>"
+        str = str + strconcat(p) + "</tr>"
+    return str
+
 
 def display(summary, friendly=False):
     if friendly:
@@ -27,12 +80,12 @@ def display(summary, friendly=False):
         fmt = null_fmt
 
     table = PrettyTable(['Broker', 'Topic', 'Partition', 'Earliest', 'Latest',
-                        'Depth', 'Spout', 'Current', 'Delta'])
+            'Depth', 'Spout', 'Current', 'Delta'])
     table.align['broker'] = 'l'
 
     for p in summary.partitions:
         table.add_row([p.broker, p.topic, p.partition, p.earliest, p.latest,
-                      fmt(p.depth), p.spout, p.current, fmt(p.delta)])
+               fmt(p.depth), p.spout, p.current, fmt(p.delta)])
     print table.get_string(sortby='Broker')
     print
     print 'Number of brokers:       %d' % summary.num_brokers
@@ -48,15 +101,15 @@ def true_or_false_option(option):
 
 def read_args():
     parser = argparse.ArgumentParser(
-        description='Show complete state of Storm-Kafka consumers')
+                    description='Show complete state of Storm-Kafka consumers')
     parser.add_argument('--zserver', default='localhost',
-        help='Zookeeper host (default: localhost)')
+                    help='Zookeeper host (default: localhost)')
     parser.add_argument('--zport', type=int, default=2181,
-        help='Zookeeper port (default: 2181)')
+                    help='Zookeeper port (default: 2181)')
     parser.add_argument('--topology', type=str, required=True,
-        help='Storm Topology')
+                    help='Storm Topology')
     parser.add_argument('--spoutroot', type=str, required=True,
-        help='Root path for Kafka Spout data in Zookeeper')
+                    help='Root path for Kafka Spout data in Zookeeper')
     parser.add_argument('--friendly', action='store_const', const=True,
                     help='Show friendlier data')
     parser.add_argument('--postjson', type=str,
@@ -64,31 +117,51 @@ def read_args():
     return parser.parse_args()
 
 def main():
+    global message
+    content = ""
+    f = open("../resource/template.html")
+    line = f.readline()
+    while line:
+        content = content + line
+        line = f.readline()
+    f.close()
     options = read_args()
 
     zc = ZkClient(options.zserver, options.zport)
     my_topology_list = zc.get_children("/")
     for single_topo in my_topology_list:
-	    print "single_topo = " + single_topo
-	    if not re.search("walle",single_topo):
-		continue
-	    consumer_partition = "/" + single_topo + "/partition_0"
-	    print "consumerid = " + consumer_partition
-	    tuple = zc.get_node(consumer_partition)[0]
-	    j = json.loads(tuple)
-	    toponame = j['topology']['name'] 
-	    print "toponame + " + toponame
-    	    try:
-        	zk_data = process(zc.spouts(consumer_partition, toponame))
-       	    except ZkError, e:
-    		print 'Failed to access Zookeeper: %s' % str(e)
-    		return 1
-    	    except ProcessorError, e:
-    		print 'Failed to process: %s' % str(e)
-    		return 1
-    	    else:
-     		display(zk_data, true_or_false_option(options.friendly))
+        api = crawlUrl + "/api/v1/topology/" + single_topo + "?sys=false"
+        read = urllib.urlopen(api).read()
+        storm_ui_data = json.loads(read)
+        if not re.search("walle",single_topo):
+            continue
+
+        consumer_partition = "/" + single_topo + "/partition_0"
+        print "consumer_partition" + consumer_partition
+        tuple = zc.get_node(consumer_partition)[0]
+        j = json.loads(tuple)
+        toponame = j['topology']['name'] 
+        try:
+            zk_data = process(zc.spouts(consumer_partition, toponame),storm_ui_data)
+        except ZkError, e:
+            print 'Failed to access Zookeeper: %s' % str(e)
+            return 1
+        except ProcessorError, e:
+            print 'Failed to process: %s' % str(e)
+            return 1
+        else:
+            display(zk_data, true_or_false_option(options.friendly))
+            print zk_data
+            content = content + mailmessage(zk_data)
     zc.stop()
+    message = message + content + htmlEnd
+    print message 
+    try:
+         smtpObj = smtplib.SMTP('localhost')
+         smtpObj.sendmail(sender, receivers, message)
+         print "Successfully sent email"
+    except SMTPException:
+         print "Error: unable to send email"
 
 if __name__ == '__main__':
     sys.exit(main())
--- stormkafkamon/processor.py
+++ stormkafkamon/processor.py
@@ -24,26 +24,36 @@ class ProcessorError(Exception):
 
 PartitionState = namedtuple('PartitionState',
     [
-        'broker',           # Broker host
-        'topic',            # Topic on broker
-        'partition',        # The partition
-        'earliest',         # Earliest offset within partition on broker
-        'latest',           # Current offset within partition on broker
-        'depth',            # Depth of partition on broker.
-        'spout',            # The Spout consuming this partition
-        'current',          # Current offset for Spout
-        'delta'             # Difference between latest and current
+         'broker',           # Broker host
+         'topic',            # Topic on broker
+         'partition',        # The partition
+         'earliest',         # Earliest offset within partition on broker
+         'latest',           # Current offset within partition on broker
+         'depth',            # Depth of partition on broker.
+         'spout',            # The Spout consuming this partition
+         'current',          # Current offset for Spout
+         'delta',             # Difference between latest and current
+         'TopologyStatsAck',
+         'TopologyStatsFailed',
+         'SpoutsAck',
+         'SpoutsFailed',
+         'FirstBoltsAck',
+         'FirstBoltsFailed',
+         'FirstBoltsProcessLatency',
+         'SecondBoltsAck',
+         'SecondBoltsFailed',
+         'SecondBoltsProcessLatency'
     ])
 PartitionsSummary = namedtuple('PartitionsSummary',
     [
-        'total_depth',      # Total queue depth.
-        'total_delta',      # Total delta across all spout tasks.
-        'num_partitions',   # Number of partitions.
-        'num_brokers',      # Number of Kafka Brokers.
-        'partitions'        # Tuple of PartitionStates
+         'total_depth',      # Total queue depth.
+         'total_delta',      # Total delta across all spout tasks.
+         'num_partitions',   # Number of partitions.
+         'num_brokers',      # Number of Kafka Brokers.
+         'partitions'        # Tuple of PartitionStates
     ])
 
-def process(spouts):
+def process(spouts,json_data):
     '''
     Returns a named tuple of type PartitionsSummary.
     '''
@@ -54,33 +64,45 @@ def process(spouts):
     for s in spouts:
         for p in s.partitions:
             try:
-		print "process function: broker host:" + p['broker']['host'] 
+                print "process function: broker host:" + p['broker']['host'] 
                 k = KafkaClient(p['broker']['host'], str(p['broker']['port']))
             except socket.gaierror, e:
                 raise ProcessorError('Failed to contact Kafka broker %s (%s)' %
-                                     (p['broker']['host'], str(e)))
-            earliest_off = OffsetRequest(str(p['topic']), p['partition'], -2, 1)
-            latest_off = OffsetRequest(str(p['topic']), p['partition'], -1, 1)
-            earliest = k.send_offset_request([earliest_off])[0]
-            latest = k.send_offset_request([latest_off])[0]
-            current = p['offset']
+                          (p['broker']['host'], str(e)))
+                earliest_off = OffsetRequest(str(p['topic']), p['partition'], -2, 1)
+                latest_off = OffsetRequest(str(p['topic']), p['partition'], -1, 1)
+                earliest = k.send_offset_request([earliest_off])[0]
+                latest = k.send_offset_request([latest_off])[0]
+                current = p['offset']
 
-            brokers.append(p['broker']['host'])
-            total_depth = total_depth + (latest.offsets[0] - earliest.offsets[0])
-            total_delta = total_delta + (latest.offsets[0] - current)
+                brokers.append(p['broker']['host'])
+                total_depth = total_depth + (latest.offsets[0] - earliest.offsets[0])
+                total_delta = total_delta + (latest.offsets[0] - current)
 
-            results.append(PartitionState._make([
-                p['broker']['host'],
-                p['topic'],
-                p['partition'],
-                earliest.offsets[0],
-                latest.offsets[0],
-                latest.offsets[0] - earliest.offsets[0],
-                s.id,
-                current,
-                latest.offsets[0] - current]))
+    results.append(PartitionState._make([
+                    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]))
+                    #p['broker']['host'],
+                    #p['topic'],
+                    #p['partition'],
+                    #earliest.offsets[0],
+                    #latest.offsets[0],
+                    #latest.offsets[0] - earliest.offsets[0],
+                    #s.id,
+                    #current,
+                    #latest.offsets[0] - current]),
+                    #json_data['topologyStats'][2]['acked'],
+                    #json_data['topologyStats'][2]['failed'],
+                    #json_data['spouts'][0]['acked'],
+                    #json_data['spouts'][0]['failed'],
+                    #json_data['bolts'][0]['acked'],
+                    #json_data['bolts'][0]['failed'],
+                    #json_data['bolts'][0]['processLatency'],
+                    #json_data['bolts'][1]['acked'],
+                    #json_data['bolts'][1]['failed'],
+                    #json_data['bolts'][1]['processLatency'])
+
     return PartitionsSummary(total_depth=total_depth,
-                             total_delta=total_delta,
-                             num_partitions=len(results),
-                             num_brokers=len(set(brokers)),
-                             partitions=tuple(results))
+                total_delta=total_delta,
+                num_partitions=len(results),
+                num_brokers=len(set(brokers)),
+                partitions=tuple(results))
